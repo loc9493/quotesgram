48113729127b53e8884f37489d10bac154125ee1
Loc Nguyen <loc.nguyen@saleshood.com>
2021-12-19 16:20:33 +0700

    slider

--- lib/view/wallpaper_slider.dart
+++ lib/view/wallpaper_slider.dart
@@ -13,25 +13,31 @@ class WallpaperSlider extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Container(
-      height: 50,
-      margin: EdgeInsets.only(top: 20),
-      child: ListView.builder(
-          scrollDirection: Axis.horizontal,
-          itemCount: vm.wallpapers.length,
-          itemBuilder: (context, index) => InkWell(
-                onTap: () => {vm.setWallpaper(vm.wallpapers[index])},
-                child: Container(
-                  height: 50,
-                  width: 100,
-                  child: Center(
-                      child: CachedNetworkImage(
-                    imageUrl: vm.wallpapers[index].url(),
-                    placeholder: (context, url) =>
-                        Center(child: CircularProgressIndicator()),
-                    errorWidget: (context, url, error) => Icon(Icons.error),
-                  )),
-                ),
-              )),
+      height: 200,
+      child: Column(
+        children: [
+          Container(
+            height: 120,
+            margin: const EdgeInsets.only(top: 20),
+            child: ListView.builder(
+                scrollDirection: Axis.horizontal,
+                itemCount: vm.wallpapers.length,
+                itemBuilder: (context, index) => InkWell(
+                      onTap: () => {vm.setWallpaper(vm.wallpapers[index])},
+                      child: Container(
+                        child: Center(
+                            child: CachedNetworkImage(
+                          imageUrl: vm.wallpapers[index].url(),
+                          placeholder: (context, url) =>
+                              const Center(child: CircularProgressIndicator()),
+                          errorWidget: (context, url, error) =>
+                              const Icon(Icons.error),
+                        )),
+                      ),
+                    )),
+          ),
+        ],
+      ),
     );
   }
 }
