4271dd0c79bf935733cd88692830d7bc5e236cc4
Nguyen Loc <loc9493@gmail.com>
2022-01-03 09:22:07 +0700

    rename package, icon

--- lib/utils/AdHelper.dart
+++ lib/utils/AdHelper.dart
@@ -8,7 +8,7 @@ class AdHelper {
     //Develop pub id
     if (kDebugMode) {
       return Platform.isAndroid
-          ? 'ca-app-pub-3940256099942544/6300978111'
+          ? 'ca-app-pub-3940256099942544/1033173712'
           : 'ca-app-pub-3940256099942544/4411468910';
     }
     //Production pub id
